26dbb33370c11a7ebdefdfcace46437885a23a12
unknown <mouatacim.med@gmail.com>
2023-09-29 02:10:35 +0200

    implemented limit on password length

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const input = document.querySelector("input")
 const generatePass = (length)=>{
     const characters = "abcdefgijklmnopqrstuvxyz0123456789"
     let pass = ""
-    for(let i = 0; i < length; i++){
-        pass += characters.charAt(Math.floor(Math.random() * characters.length))
+    if(length >= 8 && length <= 16){
+        for(let i = 0; i < length; i++){
+            pass += characters.charAt(Math.floor(Math.random() * characters.length))
+        }
+    }else{
+        return "length must be between 8 and 16"
     }
     return pass
 }
