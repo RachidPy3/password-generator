bf4f9cb292686a9f165e61b1406a3e4a21780699
unknown <mouatacim.med@gmail.com>
2023-09-29 02:04:28 +0200

    mvp done

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const btn = document.querySelector("button")
 const display = document.querySelector("h3")
 const input = document.querySelector("input")
 
+const generatePass = (length)=>{
+    const characters = "abcdefgijklmnopqrstuvxyz0123456789"
+    let pass = ""
+    for(let i = 0; i < length; i++){
+        pass += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    return pass
+}
+
+btn.addEventListener("click", ()=>{
+    let pass = generatePass(input.value)
+    display.innerText = "Your Password: " + pass
+})
